9f30442115f46d861b2011df14458c522cc41b8a
SounakShome <sounakshome@gmail.com>
2024-09-08 20:25:53 +0530

    fixed minor bugs

--- Website/crop_care/src/app/dashboard/Leftsidebar.tsx
+++ Website/crop_care/src/app/dashboard/Leftsidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
-import Logout from '@/components/logout';
+import Logout from '@/components/Logout';
 
 let links = [
     {name:"Edit Profile", link: "/"},
