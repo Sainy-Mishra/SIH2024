e8675e227f27845884c439be45a2e58ec0eb4f70
SounakShome <sounakshome@gmail.com>
2024-09-08 11:37:59 +0530

    removed not required imports

--- Website/crop_care/src/app/page.tsx
+++ Website/crop_care/src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client"
 import React from "react";
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/Vision";
-import { gsap } from "gsap";
 
 export default function Home() {
   return (
