da6d77b5858b453ee19034d2ed46eebf7337a381
SounakShome <sounakshome@gmail.com>
2024-09-08 19:31:41 +0530

    fixed logout bugs

--- Website/crop_care/src/components/logout.tsx
+++ Website/crop_care/src/components/logout.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 import { logout } from '@/app/actions/index';
 
 const Logout = () => {
+
+  const logoutGoogle = async(event: any) =>{
+    event.preventDefault();
+    await logout();
+  }
+
   return (
     <div>
         <form>
-            <button type='submit' onClick={logout}>Logout</button>
+            <button type='submit' onClick={logoutGoogle}>Logout</button>
         </form>
     </div>
   )
