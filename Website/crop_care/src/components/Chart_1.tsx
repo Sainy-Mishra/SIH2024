674310e8c2e12dd55bd1aadbb96aca2a28fa6286
Suryakiran-R <suryakiran.r8345@gmail.com>
2024-09-08 22:56:09 +0530

    DashBoard charts update

--- Website/crop_care/src/components/Chart_1.tsx
+++ Website/crop_care/src/components/Chart_1.tsx
@@ -62,7 +62,7 @@ export default function Chart_1() {
   })
 
   return (
-    <Card>
+    <Card className="my-0">
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
         <div className="grid flex-1 gap-1 text-center sm:text-left">
           <CardTitle>Area Chart - Interactive</CardTitle>
