cacd17e52121dd7b5ee48948dcca493c98e4f9f8
Suryakiran-R <suryakiran.r8345@gmail.com>
2024-09-11 08:14:58 +0530

    Dashboard update

--- Website/crop_care/src/app/dashboard/Dashboard.tsx
+++ Website/crop_care/src/app/dashboard/Dashboard.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Chart_1 from '@/components/Chart_1';
 import PieChart from '@/components/PieChart';
+
 import BarGraph from '@/components/BarGraph';
 const Dashboard = () => {
   return (
-    <div className='h-full grid grid-col-2 grid-row-2 w-full overflow-y-scroll'>
+    <div className='h-full grid md:grid-col-2 w-full overflow-y-scroll'>
           <div className='h-[50%] mb-10 col-span-2 bg-purple-400'>
-            <div className=''><Chart_1 /></div>
+            <div className='grow-2'><Chart_1 /></div>
           </div>
-          <div className=' '><PieChart /></div>
-          <div className=''><BarGraph /></div>
+          
+          <div className='flex sm:flex-col md:flex-row'>
+            <div className='grow-1'><PieChart /></div>
+            <div className='grow-1'><BarGraph /></div>
+          </div>
     </div>
   )
 }
