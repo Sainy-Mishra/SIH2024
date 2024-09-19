b94ae11281c9ccc9e28ffcb6a09ba9e946974aa4
JustCuriouss <vidyutkini@gmail.com>
2024-09-19 21:40:29 +0530

    added changes to versions of next

--- Website/crop_care/next.config.mjs
+++ Website/crop_care/next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
             protocol: 'https',
             hostname: 'lottie.host',
           },
+          {
+            protocol: 'https',
+            hostname: 'media.licdn.com',
+          },
         ],
       },
 };
--- Website/crop_care/src/data/user.ts
+++ Website/crop_care/src/data/user.ts
@@ -11,6 +11,12 @@ const user = [
     {
         email: "bJpKs@example.com",
         password: "123456"
+    },
+    {
+        name: "Vidyut Kini",
+        image: "https://media.licdn.com/dms/image/v2/D4D03AQHxOcOrQ5ZALw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1710171057621?e=1732147200&v=beta&t=EHjkWvW1z7Tjp1dEXkELcULP1T7tjT8MbiR7jHJPEa4",
+        email: "vidyut@kini.com",
+        password: "123456"
     }
 ]
 
